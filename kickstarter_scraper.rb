2edf9709257a071c247e3d9b164d14dbafa84e59
Jennifer Miller <jmillerarcher@me.com>
2020-01-27 23:17:12 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -3,7 +3,9 @@ require 'nokogiri'
 require 'pry'
 def create_project_hash
   html = File.read("fixtures/kickstarter.html")
-end 	  kickstarter = Nokogiri::HTML(html)
+end 	 
+
+kickstarter = Nokogiri::HTML(html)
 
   projects = {}
 
